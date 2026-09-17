c4c2d94248222e47b7ccdbf2148bee8386f50b10
Leftie520 <91289698+Leftie520@users.noreply.github.com>
2026-09-17 15:16:17 -0600

    Put Stamina in a Stamina namespace; rider was yelling at me and making me sad :(

--- Team Lizard/Assets/Scripts/Stamina/Stamina.cs
+++ Team Lizard/Assets/Scripts/Stamina/Stamina.cs
@@ -1,59 +1,62 @@
 using UnityEngine;
 
-public class Stamina : ScriptableObject
+namespace Stamina
 {
-    private static Stamina m_instance;
-    private float m_maxStamina;
-    private float m_currentStamina;
-
-    /// <summary>
-    /// Called once when the component is first loaded; sets itself to the singleton instance if no other stamina
-    /// bar is set, then initializes values
-    /// </summary>
-    void Awake()
+    public class Stamina : ScriptableObject
     {
-        m_instance = null;
-        m_maxStamina = 100;
-        m_currentStamina = m_maxStamina;
-    }
+        private static Stamina m_instance;
+        private float m_maxStamina;
+        private float m_currentStamina;
 
-    /// <summary>
-    /// Attempts to spend a given amount of stamina
-    /// </summary>
-    /// <param name="amount"> the amount being spent; the 'cost' of an action </param>
-    /// <returns> true if there is enough stamina to spend the specified amount, false otherwise </returns>
-    public static bool TrySpendStamina(float amount)
-    {
-        if (Instance().m_currentStamina < amount)
+        /// <summary>
+        /// Called once when the component is first loaded; sets itself to the singleton instance if no other stamina
+        /// bar is set, then initializes values
+        /// </summary>
+        void Awake()
         {
-            return false;
+            m_instance = null;
+            m_maxStamina = 100;
+            m_currentStamina = m_maxStamina;
         }
 
-        Instance().m_currentStamina -= amount;
-        return true;
-    }
-
-    /// <summary>
-    /// gets the current amount of stamina left as a percentage
-    /// </summary>
-    /// <returns> the current amount of stamina left as a percentage </returns>
-    public static float GetCurrentStaminaPercent()
-    {
-        return Instance().m_currentStamina / Instance().m_maxStamina;
-    }
-
-    /// <summary>
-    /// Accesses the singleton instance of the Stamina class used in the above functions
-    /// </summary>
-    /// <returns> the singleton instance of the Stamina class </returns>
-    public static Stamina Instance()
-    {
-        if (m_instance != null)
+        /// <summary>
+        /// Attempts to spend a given amount of stamina
+        /// </summary>
+        /// <param name="amount"> the amount being spent; the 'cost' of an action </param>
+        /// <returns> true if there is enough stamina to spend the specified amount, false otherwise </returns>
+        public static bool TrySpendStamina(float amount)
         {
+            if (Instance().m_currentStamina < amount)
+            {
+                return false;
+            }
+
+            Instance().m_currentStamina -= amount;
+            return true;
+        }
+
+        /// <summary>
+        /// gets the current amount of stamina left as a percentage
+        /// </summary>
+        /// <returns> the current amount of stamina left as a percentage </returns>
+        public static float GetCurrentStaminaPercent()
+        {
+            return Instance().m_currentStamina / Instance().m_maxStamina;
+        }
+
+        /// <summary>
+        /// Accesses the singleton instance of the Stamina class used in the above functions
+        /// </summary>
+        /// <returns> the singleton instance of the Stamina class </returns>
+        public static Stamina Instance()
+        {
+            if (m_instance != null)
+            {
+                return m_instance;
+            }
+
+            m_instance = ScriptableObject.CreateInstance<Stamina>();
             return m_instance;
         }
-
-        m_instance = ScriptableObject.CreateInstance<Stamina>();
-        return m_instance;
     }
 }
