a1fa9d8c7228890422c3f5c386a95ba27744206c
Eden Parish <101825264+weallparish@users.noreply.github.com>
2026-09-21 21:21:54 -0600

    Added struct to describe object hit by object sensor.

---
+++ Team Lizard/Assets/Scripts/Player/ObjectSensor.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public struct HitInfo
+{
+    public bool hitObstacle;
+    public RaycastHit hitData;
+    public float obstacleHeight;
+    public Quaternion targetRotation;
+}
+
+public class ObjectSensor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
