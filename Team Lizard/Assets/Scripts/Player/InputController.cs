eeed3bb3c1166bd9894d8aaafcf8ad11ac9960ef
KoizumIsaac <143127863+KoizumIsaac@users.noreply.github.com>
2026-09-24 19:19:01 -0600

    Fixed timing.

--- Team Lizard/Assets/Scripts/Player/InputController.cs
+++ Team Lizard/Assets/Scripts/Player/InputController.cs
@@ -34,7 +34,7 @@ public class InputController : MonoBehaviour
 
         [SerializeField]
         [Tooltip("How fast slide decays.")]
-        private float m_slideDecay = 0.0005f;
+        private float m_slideDecay = 1.8f;
 
         [SerializeField]
         [Tooltip("Height that player can jump.")]
@@ -114,7 +114,7 @@ public class InputController : MonoBehaviour
     {
         if (m_currentSlideMultiplier > m_minimumSlideMultiplier)
         {
-            m_currentSlideMultiplier -= m_slideDecay * (1 / Time.deltaTime);
+            m_currentSlideMultiplier -= m_slideDecay * Time.deltaTime;
         }
 
         return moveSpeed * m_currentSlideMultiplier;
